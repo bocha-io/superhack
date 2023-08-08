547441b6e160516119edeb406dd5908736b202f9
Guillermo Paoletti <guillermo.paoletti@gmail.com>
2023-08-09 00:59:47 +0200

    feat(client): init battle scene + world map (#6)
    * init battle scene
    
    * changed resolution, and setup ui
    
    * world map
    
    ---------
    
    Co-authored-by: unknown <ramirocarlucho@gmail.com>

--- frontend/Assets/Scripts/BattleMenus/HPController.cs
+++ frontend/Assets/Scripts/BattleMenus/HPController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
+using DG.Tweening;
 
 public class HPController : MonoBehaviour
 {
@@ -11,6 +12,11 @@ public class HPController : MonoBehaviour
     [SerializeField] Image _hpbar;
     int _maxHp;
     int _currentHp;
+
+    Camera _mainCamera;
+    void Start(){
+        _mainCamera = Camera.main;
+    }
 
     public void Setup(int maxHp, int currentHp){
         _maxHp = maxHp;
@@ -22,7 +28,7 @@ public class HPController : MonoBehaviour
     public void SetHp(int currentHp){
         _currentHpText.text = currentHp.ToString();
         float size = (float)_currentHp/(float)_maxHp;
-        _hpbar.gameObject.transform.localScale = new Vector3(size, 1, 1);
+        _hpbar.gameObject.transform.DOScaleX(size, 0.3f);// = new Vector3(size, 1, 1);
         if (size > 0.5f){
             _hpbar.color = Color.green;
         } else if (size < 0.15f){
@@ -37,7 +43,7 @@ public class HPController : MonoBehaviour
         if (_currentHp < 0){
             _currentHp = 0;
         }
-        
+        // _mainCamera.DOShakePosition(0.2f, fadeOut:true);
         SetHp(_currentHp);
     }
 
