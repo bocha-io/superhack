b9654ad9343576c662b652d268d4739be05c4d52
hanchon <guillermo.paoletti@gmail.com>
2023-08-08 16:21:16 +0200

    wip: create battle

--- backend/contracts-builder/contracts/src/addressToEntityKey.sol
+++ backend/contracts-builder/contracts/src/addressToEntityKey.sol
@@ -2,5 +2,11 @@
 pragma solidity >=0.8.0;
 
 function addressToEntityKey(address addr) pure returns (bytes32) {
-  return bytes32(uint256(uint160(addr)));
+    return bytes32(uint256(uint160(addr)));
 }
+
+function monKey(bytes32 playerKey, uint8 ID) pure returns (bytes32) {
+    return playerKey ^ bytes32(uint256(ID));
+}
+
+// monKey := strings.Replace(playerKey, "0x000000000000000000000000", "0x100000000000000000000000", 1)
---
+++ backend/contracts-builder/contracts/src/systems/CreateMatchSystem.sol
@@ -0,0 +1,116 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.8.0;
+
+// Core
+import {System} from "@latticexyz/world/src/System.sol";
+
+// Player options
+import {Player} from "../codegen/tables/Player.sol";
+import {Status} from "../codegen/tables/Status.sol";
+
+// Battle
+import {Match} from "../codegen/tables/Match.sol";
+import {PlayerOne} from "../codegen/tables/PlayerOne.sol";
+import {PlayerTwo} from "../codegen/tables/PlayerTwo.sol";
+
+import {Mon} from "../codegen/tables/Mon.sol";
+import {MonSpecie} from "../codegen/tables/MonSpecie.sol";
+import {MonHp} from "../codegen/tables/MonHp.sol";
+
+import {PlayerOneCurrentMon} from "../codegen/tables/PlayerOneCurrentMon.sol";
+import {PlayerTwoCurrentMon} from "../codegen/tables/PlayerTwoCurrentMon.sol";
+
+import {PlayerFirstMon} from "../codegen/tables/PlayerFirstMon.sol";
+import {PlayerSecondMon} from "../codegen/tables/PlayerSecondMon.sol";
+import {PlayerThirdMon} from "../codegen/tables/PlayerThirdMon.sol";
+
+// Inventory
+import {InventoryFirstMon} from "../codegen/tables/InventoryFirstMon.sol";
+import {InventorySecondMon} from "../codegen/tables/InventorySecondMon.sol";
+import {InventoryThirdMon} from "../codegen/tables/InventoryThirdMon.sol";
+
+// Libs
+import {StatusType, MonType} from "../codegen/Types.sol";
+import {monKey} from "../addressToEntityKey.sol";
+import {LibDefaults} from "../libs/LibDefaults.sol";
+
+contract CreateMatchSystem is System {
+    function getMonFromInventory(bytes32 player, uint8 pos) internal view returns (MonType) {
+        if (pos == 0) {
+            return InventoryFirstMon.get(player);
+        }
+        if (pos == 1) {
+            return InventorySecondMon.get(player);
+        }
+        if (pos == 2) {
+            return InventoryThirdMon.get(player);
+        }
+        require(false, "invalid pos");
+    }
+
+    function setPlayerCurrentMon(bytes32 gameKey, uint8 player, bytes32 monID) internal {
+        if (player == 0) {
+            PlayerOneCurrentMon.set(gameKey, monID);
+            return;
+        }
+        if (player == 1) {
+            PlayerTwoCurrentMon.set(gameKey, monID);
+            return;
+        }
+        require(false, "invalid player");
+    }
+
+    function setGameMon(bytes32 player, uint8 pos, bytes32 monID) internal {
+        if (pos == 0) {
+            PlayerFirstMon.set(player, monID);
+            return;
+        }
+        if (pos == 1) {
+            PlayerSecondMon.set(player, monID);
+            return;
+        }
+        if (pos == 2) {
+            PlayerThirdMon.set(player, monID);
+            return;
+        }
+        require(false, "invalid pos");
+    }
+
+    function CreateMatch(bytes32 playerA, bytes32 playerB) public {
+        // TODO: allow only game admin keys to access to this function!
+
+        // Make sure that the player is registered
+        require(Player.get(playerA) == true, "player a is not registered");
+        require(Player.get(playerB) == true, "player b is not registered");
+
+        // The user can play one game at the time
+        require(Status.get(playerA) == StatusType.Walking, "player a is already in a match");
+        require(Status.get(playerB) == StatusType.Walking, "player b is already in a match");
+
+        // Game key will be the same as playerA
+        bytes32 gameKey = playerA;
+
+        // CreateMatch
+        Match.set(gameKey, true);
+        PlayerOne.set(gameKey, playerA);
+        PlayerTwo.set(gameKey, playerB);
+
+        // Create Mons
+        for (uint8 player = 0; player < 2; player++) {
+            bytes32 playerKey = playerA;
+            if (player == 1) {
+                playerKey = playerB;
+            }
+            for (uint8 i = 0; i < 3; i++) {
+                bytes32 key = monKey(playerKey, i);
+                Mon.set(key, true);
+                MonSpecie.set(key, getMonFromInventory(playerKey, i));
+                MonHp.set(key, LibDefaults.getHp(getMonFromInventory(playerKey, i)));
+                setGameMon(playerKey, i, key);
+                if (i == 0) {
+                    setPlayerCurrentMon(gameKey, player, key);
+                }
+            }
+        }
+    }
+}
