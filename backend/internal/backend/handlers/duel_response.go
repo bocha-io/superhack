1555524914cb65b3350640419170d049113dfd32
hanchon <guillermo.paoletti@gmail.com>
2023-08-11 17:30:07 +0200

    fix: allow players to decline a duel

--- backend/internal/backend/handlers/duel_response.go
+++ backend/internal/backend/handlers/duel_response.go
@@ -17,6 +17,8 @@ type DuelResponseMessage struct {
 type DuelResponseMessageResponse struct {
 	MsgType string `json:"msgtype"`
 	Value   Duel   `json:"value"`
+	Status  string `json:"status"`
+	PlayerA string `json:"matchid"`
 	Error   string `json:"error"`
 }
 
@@ -33,11 +35,17 @@ func NewDuelResponseMessage(playerA string, response bool) DuelResponseMessage {
 	}
 }
 
-func newDuelResponseMessageResponse(duel Duel) DuelResponseMessageResponse {
+func newDuelResponseMessageResponse(
+	duel Duel,
+	status string,
+	playerA string,
+) DuelResponseMessageResponse {
 	return DuelResponseMessageResponse{
 		MsgType: DuelResponseMessageResponseID,
 		Value:   duel,
 		Error:   "",
+		Status:  status,
+		PlayerA: playerA,
 	}
 }
 
@@ -61,6 +69,8 @@ func (b *Backend) duelResponseMessage(
 			PlayerA: duelresponseMsg.PlayerA,
 			PlayerB: ws.WalletAddress,
 		},
+		"accepted",
+		duelresponseMsg.PlayerA,
 	)
 
 	// Check if the match was created in the server
@@ -69,7 +79,11 @@ func (b *Backend) duelResponseMessage(
 		_ = messages.WriteJSON(
 			ws.Conn,
 			ws.ConnMutex,
-			newDuelResponseMessageResponse(Duel{PlayerA: "", PlayerB: ""}),
+			newDuelResponseMessageResponse(
+				Duel{PlayerA: "", PlayerB: ""},
+				"match not created",
+				duelresponseMsg.PlayerA,
+			),
 		)
 		return nil
 	}
@@ -79,32 +93,65 @@ func (b *Backend) duelResponseMessage(
 		_ = messages.WriteJSON(
 			ws.Conn,
 			ws.ConnMutex,
-			newDuelResponseMessageResponse(Duel{PlayerA: "", PlayerB: ""}),
+			newDuelResponseMessageResponse(
+				Duel{PlayerA: "", PlayerB: ""},
+				"invalid player",
+				duelresponseMsg.PlayerA,
+			),
 		)
+		return nil
+	}
+
+	if !duelresponseMsg.Response {
+		response.Value.PlayerA = ""
+		response.Value.PlayerB = ""
+		response.Status = "rejected"
+
+		// This just removes the match from the list
+		b.gameAdmins.AcceptMatchRequest(duelresponseMsg.PlayerA)
+
+		pA := b.GetConex(response.Value.PlayerA)
+		if pA != nil {
+			_ = messages.WriteJSON(pA.Conn, pA.ConnMutex, response)
+		}
+		pB := b.GetConex(response.Value.PlayerB)
+		if pB != nil {
+			_ = messages.WriteJSON(pB.Conn, pB.ConnMutex, response)
+		}
+
 		return nil
 	}
 
 	// Accept the match
 	b.gameAdmins.AcceptMatchRequest(response.Value.PlayerA)
-	// Inform the two player that the game is starting
+
 	pA := b.GetConex(response.Value.PlayerA)
-	if pA != nil {
-		_ = messages.WriteJSON(pA.Conn, pA.ConnMutex, response)
-	}
 	pB := b.GetConex(response.Value.PlayerB)
-	if pB != nil {
-		_ = messages.WriteJSON(pB.Conn, pB.ConnMutex, response)
-	}
 
 	// Create a match
 	matchMsg, err := createMatch(b, response.Value.PlayerA, response.Value.PlayerB)
 	if err != nil {
-		_ = messages.WriteJSON(
-			ws.Conn,
-			ws.ConnMutex,
-			newDuelResponseMessageResponse(Duel{PlayerA: "", PlayerB: ""}),
+		// Error creating match
+		resp := newDuelResponseMessageResponse(
+			Duel{PlayerA: "", PlayerB: ""},
+			"error creating match",
+			duelresponseMsg.PlayerA,
 		)
+		if pA != nil {
+			_ = messages.WriteJSON(pA.Conn, pA.ConnMutex, resp)
+		}
+		if pB != nil {
+			_ = messages.WriteJSON(pB.Conn, pB.ConnMutex, resp)
+		}
 		return nil
+	}
+
+	// Inform the two player that the game is starting
+	if pA != nil {
+		_ = messages.WriteJSON(pA.Conn, pA.ConnMutex, response)
+	}
+	if pB != nil {
+		_ = messages.WriteJSON(pB.Conn, pB.ConnMutex, response)
 	}
 
 	// Broadcast the matchID
@@ -123,7 +170,12 @@ func (b *Backend) duelResponseMessage(
 	)
 
 	// Broadcast the match info
-	b.broadcastMatchState(matchMsg.Value.MatchID, matchMsg.Value.PlayerOne, matchMsg.Value.PlayerTwo, Actions{})
+	b.broadcastMatchState(
+		matchMsg.Value.MatchID,
+		matchMsg.Value.PlayerOne,
+		matchMsg.Value.PlayerTwo,
+		Actions{},
+	)
 
 	return nil
 }
