a8afeaa91ba92d75fa4f0b85bdab853805215f37
Guillermo Paoletti <guillermo.paoletti@gmail.com>
2023-08-12 21:41:54 +0200

    feat: player inventory (#20)
    * feat: player inventory
    
    * fix: linter issue

---
+++ backend/internal/backend/handlers/player_inventory.go
@@ -0,0 +1,91 @@
+package handlers
+
+import (
+	"encoding/json"
+	"fmt"
+	"strings"
+
+	"github.com/bocha-io/game-backend/x/messages"
+)
+
+type InventoryMessage struct {
+	MsgType string `json:"msgtype"`
+}
+
+type InventoryMessageResponse struct {
+	MsgType string  `json:"msgtype"`
+	Value   []int64 `json:"value"`
+	Error   string  `json:"error"`
+}
+
+const (
+	InventoryMessageType       = "inventory"
+	InventoryMessageResponseID = "inventoryresponse"
+)
+
+func NewInventoryMessage() InventoryMessage {
+	return InventoryMessage{
+		MsgType: InventoryMessageType,
+	}
+}
+
+func newInventoryMessageError(err error) InventoryMessageResponse {
+	return InventoryMessageResponse{
+		MsgType: InventoryMessageResponseID,
+		Value:   []int64{},
+		Error:   err.Error(),
+	}
+}
+
+func newInventoryMessageResponse(mons []int64) InventoryMessageResponse {
+	return InventoryMessageResponse{
+		MsgType: InventoryMessageResponseID,
+		Value:   mons,
+		Error:   "",
+	}
+}
+
+func (b *Backend) inventoryMessage(
+	ws *messages.WebSocketContainer,
+	p []byte,
+) (resp InventoryMessageResponse, err error) {
+	// The prediction will panic if something fails in the database, catch it here
+	defer func() {
+		if r := recover(); r != nil {
+			err = fmt.Errorf("error getting info from the database")
+			resp = InventoryMessageResponse{}
+		}
+	}()
+
+	if !ws.Authenticated {
+		return InventoryMessageResponse{}, fmt.Errorf("user is not logged in")
+	}
+
+	var inventoryMsg InventoryMessage
+	err = json.Unmarshal(p, &inventoryMsg)
+	if err != nil {
+		return newInventoryMessageError(err), err
+	}
+
+	res := make([]int64, 3)
+	temp := strings.Replace(ws.WalletAddress, "0x", "0x000000000000000000000000", 1)
+	mon, err := b.queryClient.GetInventoryFirstMon(temp)
+	if err != nil {
+		return newInventoryMessageResponse([]int64{}), nil
+	}
+	res = append(res, mon)
+
+	mon, err = b.queryClient.GetInventorySecondMon(temp)
+	if err != nil {
+		return newInventoryMessageResponse([]int64{}), nil
+	}
+	res = append(res, mon)
+
+	mon, err = b.queryClient.GetInventoryThirdMon(temp)
+	if err != nil {
+		return newInventoryMessageResponse([]int64{}), nil
+	}
+	res = append(res, mon)
+
+	return newInventoryMessageResponse(res), nil
+}
--- backend/internal/backend/handlers/server.go
+++ backend/internal/backend/handlers/server.go
@@ -141,6 +141,13 @@ func (b *Backend) HandleMessage(
 			return nil
 		}
 
+	case InventoryMessageType:
+		response, err := b.inventoryMessage(ws, p)
+		if err != nil {
+			return err
+		}
+		return messages.WriteJSON(ws.Conn, ws.ConnMutex, response)
+
 	case SwapMessageType:
 		if response, err := b.swapMessage(ws, p); err != nil {
 			return err
